97f393ce567f5d5ca6d8ee31647b6230918455a2
Nelson Nguyen <nelsonnguyen@Mac.lan>
2025-02-22 20:56:12 -0800

    Added logo section in index.html, created a message schema, and message function container

--- backend/controllers.js
+++ backend/controllers.js
@@ -1,23 +1,49 @@
-const User = require('./models')
+const User = require("./models");
 
+// USER controller
 const RegisterNewUser = async (req, res) => {
-    //TODO: add confirm password field
-    const {email, password, first_name, last_name} = req.body
-    const id = 2
-    try {
-        const user = await User.create({
-            email: email,
-            password: password,
-            first_name: first_name,
-            last_name: last_name,
-            uid: 2
-        })
-        res.status(200).json(user)
-    } catch (error) {
-        res.status(400).json({error: error.message})
-    }
-}
+  //TODO: add confirm password field
+  const { email, password, first_name, last_name } = req.body;
+  const id = 2;
+  try {
+    const user = await User.create({
+      email: email,
+      password: password,
+      first_name: first_name,
+      last_name: last_name,
+      uid: 2,
+    });
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 
 module.exports = {
-    RegisterNewUser: RegisterNewUser
+  RegisterNewUser: RegisterNewUser,
+};
+
+// MESSAGE controller
+const Message = async (req, res) => {
+  const { sender, receiver, content } = req.body;
+
+  try {
+    const newMessage = new Message({ sender, receiver, content });
+    await newMessage.save();
+
+    res
+      .status(200)
+      .json({ message: "Message sent succesfully", data: newMessage });
+  } catch (error) {
+    res.status(400).json({ error: "Failed to send message" });
+  }
+};
+
+/* not sure if this is how we're suppose to be exporting multiple controller functions
+module.exports = {
+    RegisterNewUser: RegisterNewUser;
+    Message: Message;
+
+
 }
+*/
--- backend/models.js
+++ backend/models.js
@@ -11,3 +11,20 @@ const user_schema = new schema({
 }, {timestamps: true})
 
 module.exports = mongoose.model('User', user_schema)
+
+
+// sender = UserID
+// receiver = UserID
+// content = message
+// timestamp = time of message sent
+// status = "sent, delivered, or read"
+const message_schema = new schema({
+  sender: {type: mongoose.Schema.Types.ObjectId, ref: "User", required: true}, 
+  receiver: {type: mongoose.Schema.Types.ObjectId, ref: "User", required: true},
+  content: {type: string, required: true},
+  timestamp: {type: Date, default: Date.now},
+  status: {type: string, enum: ["sent", "delivered", "read"], defualt: "sent"}
+});
+
+const Message = mongoose.model("Message", message_schema);
+module.exports = Message;
