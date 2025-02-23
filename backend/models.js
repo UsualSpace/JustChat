e8c36c7efaa6015c5ecd1321e69296b29ae7083d
UsualSpace <70238647+UsualSpace@users.noreply.github.com>
2025-02-23 14:56:54 -0800

    Schema's update, untested

--- backend/models.js
+++ backend/models.js
@@ -1,30 +1,44 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-const schema = mongoose.Schema 
+//User related schemas.
+const user_schema = new mongoose.Schema({
+  email: {type: String, required: true, unique: true},
+  password: {type: String, required: true},
+  first_name: {type: String, required: true},
+  last_name: {type: String, required: true},
+}, {timestamps: true});
 
-const user_schema = new schema({
-    email: {type: String, required: true},
-    password: {type: String, required: true},
-    first_name: {type: String, required: true},
-    last_name: {type: String, required: true},
-    uid: {type: Number, required: true}
-}, {timestamps: true})
-
-module.exports = mongoose.model('User', user_schema)
-
-
-// sender = UserID
-// receiver = UserID
-// content = message
-// timestamp = time of message sent
-// status = "sent, delivered, or read"
-const message_schema = new schema({
-  sender: {type: mongoose.Schema.Types.ObjectId, ref: "User", required: true}, 
+const friendship_schema = new mongoose.Schema({
+  requester: {type: mongoose.Schema.Types.ObjectId, ref: "User", required: true},
   receiver: {type: mongoose.Schema.Types.ObjectId, ref: "User", required: true},
-  content: {type: string, required: true},
-  timestamp: {type: Date, default: Date.now},
-  status: {type: string, enum: ["sent", "delivered", "read"], defualt: "sent"}
+  status: {type: String, enum: ["accepted", "pending"], default: "pending"}
+}, {timestamps: true});
+
+//Chat related schemas.
+const member_schema = new mongoose.Schema({
+  user: {type: mongoose.Schema.Types.ObjectId, ref: "User", required: true},
+  role: {type: String, enum: ["normal", "moderator", "owner"], default: "normal"}
 });
 
-const Message = mongoose.model("Message", message_schema);
-module.exports = Message;
+const message_schema = new mongoose.Schema({
+  sender: {type: mongoose.Schema.Types.ObjectId, ref: "User", required: true}, 
+  receiver: {type: mongoose.Schema.Types.ObjectId, ref: "User", required: true},
+  content: {type: String, required: true},
+}, {timestamps: true});
+
+const groupchat_schema = new mongoose.Schema({
+  name: {type: String, required: true},
+  members: [member_schema],
+  messages: [message_schema],
+  censored_phrases: [{type: String}]
+}, {timestamps: true});
+
+const User = mongoose.model('User', user_schema);
+const Friendship = mongoose.model('FriendShip', friendship_schema);
+const GroupChat = mongoose.model("GroupChat", groupchat_schema);
+
+module.exports = {
+  User,
+  Friendship,
+  GroupChat
+};
