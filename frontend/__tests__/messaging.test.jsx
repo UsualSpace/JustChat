a547e327c75e12e33985aca9c915f6d70f893d8b
Nelson Nguyen <nelsonnguyen9878@gmail.com>
2025-03-18 14:22:55 -0700

    messaging.test.jsx, setsuccess, seterror

---
+++ frontend/__tests__/messaging.test.jsx
@@ -0,0 +1,24 @@
+import axios from "axios";
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom";
+import Messaging from "../src/pages/messaging";
+
+jest.mock("axios");
+
+describe("Messaging page", () => {
+    beforeAll(async () => {
+        
+    });
+
+    afterAll(async () => {
+        
+    });
+
+    beforeEach(async () => {
+        jest.clearAllMocks();
+    });
+
+    Test("display '")
+    
+})
